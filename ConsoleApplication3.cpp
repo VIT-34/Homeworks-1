5866ab2b0beee7a1ce35dbc6f66df963e48734b2
Vitaliy <viurum986@mail.ru>
2021-12-20 22:23:58 +0300

    ConsoleApplication3 images1 directory

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int a = 30;
+    int b = 10;
+    int c = 40;
+    int d = 33;
+    float n = a * (b + (float) c / d);
+
+    cout << n << endl;
+}
+
+// 2 задание
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 80;
+    int y = 21;
+    int n = x - y;
+    int i = (n * 2);
+    cout << n << endl;
+    cout << i << endl;
+    if (n > y);
+
+    else (n < y);
+    cout << (n > y) ? i : n;
+
+};
